cec53ef8555fd2394349f9e1f592e9e6e0ddd2d0
TrufflePhantom <znak.05@mail.ru>
2021-04-11 20:57:12 +0300

    first commit

--- 1/src/index.js
+++ 1/src/index.js
@@ -1,2 +1,34 @@
+function checkcompare(num) {
+  let compare = prompt("какое число от 1 до 10 загадали?");
+  console.log(`Число которое вы ввели: ${compare}`);
+  if (num === compare) {
+    alert("Ура!!!! Вы отгадали");
+  } else {
+    checkcompare(num);
+  }
+}
 
+function checknumber(number) {
+  if (number > 0 && number <= 10) {
+    checkcompare(number);
+  } else {
+    alert("вы ввели не правильное число");
+    startApp();
+  }
+}
 
+function startApp() {
+  let number = prompt("загадайте число от 1 до 10");
+  if (number) {
+    checknumber(number);
+  } else {
+    let stopgame = confirm("вы точно хотите закончить игру? ");
+    if (stopgame) {
+      return;
+    } else {
+      startApp();
+    }
+  }
+  console.log(`Число которое вы загадали: ${number}`);
+}
+startApp();
