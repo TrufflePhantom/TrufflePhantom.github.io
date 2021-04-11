6f414d6397cf04c4c196cc748ff8a94f79639770
TrufflePhantom <znak.05@mail.ru>
2021-04-11 21:08:56 +0300

    first commit

--- 1/src/index.js
+++ 1/src/index.js
@@ -1,8 +1,8 @@
 function checkcompare(num) {
-  let compare = prompt("какое число от 1 до 10 загадали?");
+  let compare = prompt("ля, какое число загадал предыдущий комрад?");
   console.log(`Число которое вы ввели: ${compare}`);
   if (num === compare) {
-    alert("Ура!!!! Вы отгадали");
+    alert("Ты шо маг?! Ты отгадал!");
   } else {
     checkcompare(num);
   }
@@ -12,17 +12,17 @@ function checknumber(number) {
   if (number > 0 && number <= 10) {
     checkcompare(number);
   } else {
-    alert("вы ввели не правильное число");
+    alert("ты шо чык-чырык?! от 1 до 10!");
     startApp();
   }
 }
 
 function startApp() {
-  let number = prompt("загадайте число от 1 до 10");
+  let number = prompt("Ля, загадай чиcло от 1 до 10");
   if (number) {
     checknumber(number);
   } else {
-    let stopgame = confirm("вы точно хотите закончить игру? ");
+    let stopgame = confirm("ля ты уже закончил играть? ");
     if (stopgame) {
       return;
     } else {
